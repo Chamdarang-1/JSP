418f06f6edd29bbeac7f473d76b8f087e124386f
Chamdarang-1 <joseph8034@naver.com>
2025-07-31 12:31:36 +0900

    ch08

---
+++ ch08/src/main/java/sub3/MyContextListener.java
@@ -0,0 +1,27 @@
+package sub3;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+public class MyContextListener implements ServletContextListener {
+	// WAS의 시작과 종료를 감시하는 리스너 
+	
+	public MyContextListener() {
+		System.out.println("MyContextListner 생성...");
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println("MyContextListner contextInitialized...");
+	}
+	
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		System.out.println("MyContextListner contextDestroyed...");
+	
+	
+	
+	}
+}
+
